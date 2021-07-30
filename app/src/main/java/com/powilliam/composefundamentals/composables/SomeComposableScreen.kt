d4d444bcfd8748c626ec6b1d9d0cb5798b508de8
William Porto <william.porto@nave.rs>
2021-07-30 01:12:51 -0300

    chore(app): more annotations abouut compose

--- app/src/main/java/com/powilliam/composefundamentals/composables/SomeComposableScreen.kt
+++ app/src/main/java/com/powilliam/composefundamentals/composables/SomeComposableScreen.kt
@@ -22,7 +22,16 @@ import com.powilliam.composefundamentals.ui.ComposeFundamentalsTheme
 2.  By default, @Composable functions doesn't return anything.
 3.  Normally, when a @Composable function returns any data, it's a way to do some calculation.
 4.  @Composable functions can receive parameters. They receive it as input and uses it to render UI or resolve calculations as output.
+5.  In fact, when a @Composable function receives another input,
+    they will be re-rendered from scratch. This process is called RECOMPOSITION.
+6.  @Composable functions are flexible to the Kotlin language. We can use since if else statements to for loops.
 */
+
+/*
+* 1. @Composable functions can execute in any order
+* 2. @Composable functions can execute in parallel
+* 3. @Composable functions can be skyped (normally when its parent suffers recompositions but they doesn't need to be redraw)
+* */
 @Composable
 fun SomeComposableScreen(
     uiState: State<Int>,
